24da5ed5a9101ba2ec0b1d81415c3e5254e3c5e9
sadiyaj <sadiyajiru@gmail.com>
2018-08-06 22:17:22 -0500

    timer works, all css/formating is good, results page is not printing results

---
+++ assets/javascript/app.js
@@ -0,0 +1,103 @@
+var number = 60;
+var intervalId;
+function start() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000); //1 second decroment
+}
+function decrement() {
+  number--;
+  //  Show the number in the #timer tag.
+  $("#timer").html("<h2> Seconds Left: " + number + "</h2>");
+
+  //  Once number hits zero...
+  if (number === 0) {
+    //  .. start the stop function.
+    stop();
+    //  Alert the user that time is up.
+    alert("Time Up!");
+    // alert("you got " + correct + " questions right" + " & you got " + incorrect + " questions wrong")
+    // check();
+  }
+}
+//  The stop function
+function stop() {
+  // alert("Times UP! you got " + correct + " questions right" + " & you got " + incorrect + " questions wrong")
+  //  Clears our intervalId
+  //  We just pass the name of the interval
+  //  to the clearInterval function.
+  clearInterval(intervalId);
+}
+//  Execute the start function.
+start();
+
+function check() {
+  var correct = 0;
+  var incorrect = 0;
+
+  var question1 = document.quiz.question1.value;
+  var question2 = document.quiz.question2.value;
+  var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
+  var question5 = document.quiz.question5.value;
+  var question6 = document.quiz.question6.value;
+
+
+  if (question1 == 54) {
+    correct++;
+  } else {
+    incorrect++;
+  }
+  if (question2 == "Addis Ababa") {
+    correct++;
+  } else {
+    incorrect++;
+  }
+  if (question3 == "Kenya") {
+    correct++;
+  } else {
+    incorrect++;
+  }
+  if (question4 == "Cheetah") {
+    correct++;
+  } else {
+    incorrect++;
+  }
+  if (question5 == "Arabic") {
+    correct++;
+  } else {
+    incorrect++;
+  }
+  if (question6 == "85%") {
+    correct++;
+  } else {
+    incorrect++;
+  }
+
+
+
+
+
+  alert(
+    "you got " +
+      correct +
+      " questions right" +
+      " & you got " +
+      incorrect +
+      " questions wrong"
+  );
+
+  $("#afterSubmit").attr("style", "visibility: visible");
+  $("#correctNum").html("you got " + correct + " questions right");
+  $("#incorrectNum").html("you got " + incorrect + " questions wrong");
+
+
+  stop();
+  clearInterval(intervalId);
+}
+// $("#button").click(function() {
+// // //   $(this).data("clicked", true);
+// //   $("#timer").attr("style", "visibility: hide");
+
+//   stop();
+//   clearInterval(intervalId);
+// });
